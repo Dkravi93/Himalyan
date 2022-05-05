9c857f84c3df9a67793654024dd645ad807dae02
DeepakRavi <94233440+Dkravi93@users.noreply.github.com>
2022-05-05 10:20:21 +0530

    changes

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 27017),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS',['fafPWsg4LV4WqCqzKktF/w==' ,'UQgMm0TpCu9tN2IHJrieiQ==','IZefquJoyB4gRQBKqHSj4A==' ,'cYVQXKZiaZU4l2jCvOQ6wg==']),
   },
 });
